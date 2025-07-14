6e6e1c668f9c06d8f0e76d687748fb2a8de605d2
Maximilien <maximilien.zakowski@gmail.com>
2025-07-14 09:55:56 +0200

    publication du rendu

--- src/components.d.ts
+++ src/components.d.ts
@@ -8,7 +8,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     QrCodeGenerator: typeof import('./components/QrCodeGenerator.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
